61d22f56114fab167984b399398e8ed8d5931311
TODO <TODO>
2024-03-13 15:50:30 +0100

    caneva non modif en comentaire

--- tp-solid/canevas/src/Forme.cpp
+++ tp-solid/canevas/src/Forme.cpp
@@ -4,6 +4,56 @@ Disque::Disque(double rayon) :
     _rayon(rayon)
 {}
 
+float Disque::aire() const {
+    return _rayon * 2 * M_PI;
+}
+
+Description Disque::toDescription() const {
+    Description des;
+
+    /*
+    os << "  {\n";
+    os << "    \"name\": \"Disque\",\n";
+    os << "    \"rayon\": \"" << disque->_rayon << "\"\n";
+    os << "  }";*/
+    return des;
+}
+
 Rectangle::Rectangle(double largeur, double hauteur) :
     _largeur(largeur), _hauteur(hauteur)
 {}
+
+float Rectangle::aire() const {
+    return _largeur * _hauteur;
+}
+
+Description Rectangle::toDescription() const {/*
+    os << "  {\n";
+    os << "    \"name\": \"Rectangle\",\n";
+    os << "    \"largeur\": \"" << rectangle->_largeur << "\",\n";
+    os << "    \"hauteur\": \"" << rectangle->_hauteur << "\"\n";
+    os << "  }";*/
+}
+
+
+
+/*
+void Canevas::exportFormeJSON(std::ostream & os, const Forme * f) const {
+
+    auto disque = dynamic_cast<const Disque*>(f);
+    if (disque) {
+        os << "  {\n";
+        os << "    \"name\": \"Disque\",\n";
+        os << "    \"rayon\": \"" << disque->_rayon << "\"\n";
+        os << "  }";
+        return;
+    }
+
+    auto rectangle = dynamic_cast<const Rectangle*>(f);
+    if (rectangle) {
+        
+        return;
+    }
+
+}
+*/
--- tp-solid/canevas/src/Forme.hpp
+++ tp-solid/canevas/src/Forme.hpp
@@ -1,20 +1,41 @@
 #pragma once
 
-class Forme {
+#include <cmath>
+#include <numeric>
+#include <map>
+
+
+using Description = std::map<std::string, std::string>;
+
+class Descriptible {
+    virtual Description toDescription() const = 0;
+};
+
+class Forme: public Descriptible {
     public:
         virtual ~Forme() = default;
+        virtual float aire() const = 0;
 };
 
 class Disque : public Forme {
-    public:
+    private:
         double _rayon;
+    public:
+
         Disque(double rayon);
+        float aire() const override;
+        Description toDescription() const override;
 };
 
 class Rectangle : public Forme {
-    public:
+    private:
         double _largeur;
         double _hauteur;
+
+    public:
         Rectangle(double largeur, double hauteur);
+        float aire() const override;
+        Description toDescription() const override;
+
 };
 
