85c070795c5b99ac87e74eb7bf37e497b2837436
TODO <TODO>
2024-04-18 14:56:58 +0200

    Milestone-2

--- tp-workflow/cpp/src/tictactoe-cli.cpp
+++ tp-workflow/cpp/src/tictactoe-cli.cpp
@@ -1,23 +1,52 @@
 #include "Tictactoe.hpp"
+#include <iostream>
 
 using namespace std;
 
 int main() {
     Jeu jeu;
 
-    cout << jeu << endl;
+    std::string cli;
+    int i, j;
 
+    //while (true) { //Décomenter pour partie infinie
+
+        while (jeu.getStatus() == Status::RougeJoue or jeu.getStatus() == Status::VertJoue) {
+            std::cout << "Nouvelle patrtie :" << std::endl;
+            std::cout << jeu << std::endl;
+
+            if (jeu.getStatus() == Status::RougeJoue) {
+                std::cout << "rouge joue" << std::endl;
+            } else {
+                std::cout << "vert joue" << std::endl;
+            }
+
+            std::cout << "Entrer une position ex(1 1) : ";
+            std::getline(std::cin, cli);
+
+            if (cli.length() < 2 or cli.length() > 4) {
+                std::cout << "Entrer incorect" << std::endl;
+            } else {
+                i = cli[0] - '0';
+                j = cli[2] - '0';
+                std::cout << i << ";" << j << std::endl;
+                if (!jeu.jouer(i, j)) {
+                   std::cout << "Coup  invalide" << std::endl;
+                }
+            }
+        }
+
+        if (jeu.getStatus() == Status::Egalite) {
+            std::cout << "Partie fini : Egalité" << std::endl;
+        } else if (jeu.getStatus() == Status::RougeGagne) {
+            std::cout << "Partie fini : Victoire de rouge" << std::endl;
+        } else {
+            std::cout << "Partie fini : Victoire de vert" << std::endl;
+        }
+
+        jeu.raz();
+    //}
     
-    jeu.jouer(0, 0);
-    cout << jeu << endl;
-    jeu.jouer(0, 1);
-    cout << jeu << endl;
-    jeu.jouer(1, 0);
-    cout << jeu << endl;
-    jeu.jouer(0, 2);
-    cout << jeu << endl;
-    jeu.jouer(2, 0);
-    cout << jeu << endl;
 
     return 0;
 }
